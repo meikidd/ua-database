ca0bd55b1ffd1fd4972ad573c973cee68b6e5e85
qingguang.meiqg <qingguang.meiqg@alibaba-inc.com>
2016-04-24 17:57:28 +0800

    read and write file

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+var fs = require('fs');
 var koa = require('koa');
 
 var app = koa();
-
 app.use(function *() {
-  this.body = 'hello world';
-})
+  var filePath = __dirname+'/ua-data.json';
+  var uaData = fs.readFileSync(filePath);
+  uaData = JSON.parse(uaData);
+  console.log(uaData);
+  uaData.push({ua:"heihei"});
+  console.log(uaData);
 
-app.listen(process.env.PORT, function() {
-  console.log('server started on:80');
+  fs.writeFileSync(filePath, JSON.stringify(uaData));
+  this.body = JSON.stringify(uaData);
+});
+
+app.listen(process.env.PORT || 7001, function() {
+  console.log('server started on: 7001');
 });
